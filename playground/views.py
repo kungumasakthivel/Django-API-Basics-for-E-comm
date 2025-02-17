d0a1b1e6d401bf8a5fb60bee4d6ccfebd7614d19
kungumasakthivel <kungumasakthivel.k@gmail.com>
2025-02-17 11:05:05 +0530

    render template html file

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from django.template import loader
 
 def say_hello(request):
     return HttpResponse('Hello World')
+
+def say_hello(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
